8d3950c6e12a2d1ee63c0a2f1eafe933a48e8437
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-03-29 13:53:31 -0400

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,16 @@
+// This program prnits Welcome to Java on the screen
+
+/* 
+My First Java App
+Developer: Robert J Ferdarko Jr
+March 29, 2021
+
+*/
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Java!");
+	}
+	
+}
