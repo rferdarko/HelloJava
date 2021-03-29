df08acb098f99d052a5cd3fe96d288b88fe1cfe4
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-03-29 19:27:17 -0400

    First Commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
+
+        double area = 2.9, ceiling = 3.5;
+        System.out.println(area);
+
+
+        Scanner input = new Scanner(System.in);
+                // ctl + space will tell you what you can put into Scanner constructor
     }
 }
